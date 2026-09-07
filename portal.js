960fdf99192db066362737ce2e8718d632663396
Mythos Mondays <christopher.milosch@web.de>
2026-09-07 21:54:48 +0200

    Load updated navigation font sizing

--- portal.js
+++ portal.js
@@ -1,10 +1,10 @@
 (() => {
   const navStyle = document.querySelector('link[href*="nav-overlay.css"]');
-  if (navStyle) navStyle.href = './nav-overlay.css?v=8';
+  if (navStyle) navStyle.href = './nav-overlay.css?v=9';
   else {
     const link = document.createElement('link');
     link.rel = 'stylesheet';
-    link.href = './nav-overlay.css?v=8';
+    link.href = './nav-overlay.css?v=9';
     document.head.appendChild(link);
   }
 
